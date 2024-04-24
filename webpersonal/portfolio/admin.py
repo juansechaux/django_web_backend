29c31e32f0786cf9fa591a31d3d5706a6e86347a
juansechaux <juansechaux@hotmail.com>
2024-04-24 16:54:22 -0500

    create a admin user, move the portfolio.html from core to portfolio, update the views.py from portfolio(create the view for portfolio) and core(delete the view), update the portfolio.html to get the data from de db.

--- webpersonal/portfolio/admin.py
+++ webpersonal/portfolio/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Project
+
 
 # Register your models here.
+class ProjectAdmin(admin.ModelAdmin):
+    readonly_fields = ('created_At', 'updated_At')
+
+
+
+admin.site.register(Project, ProjectAdmin)
--- webpersonal/portfolio/models.py
+++ webpersonal/portfolio/models.py
@@ -3,8 +3,17 @@ from django.db import models
 # Create your models here.
 
 class Project(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.TextField(max_length=2500)
-    image = models.ImageField()
-    created_At = models.DateTimeField(auto_now_add=True)
-    updated_At = models.DateTimeField(auto_now=True)
+    title = models.CharField(max_length=100, verbose_name="Titulo")
+    description = models.TextField(max_length=2500, verbose_name="Descripción")
+    image = models.ImageField(verbose_name="Imagen", upload_to="projects")
+    created_At = models.DateTimeField(auto_now_add=True, verbose_name="Fecha de creacion")
+    updated_At = models.DateTimeField(auto_now=True, verbose_name="Fecha de Actualizacion")
+
+    class Meta:
+        verbose_name = "proyecto"
+        verbose_name_plural = "proyectos"
+        ordering = ['-created_At']
+    
+    def __str__(self):
+        return self.title
+
